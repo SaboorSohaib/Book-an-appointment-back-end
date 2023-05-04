e1aef029e61ddbe4b01ba4a37b6411c67c0e272b
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-04 18:40:59 +0430

    ADD ROUTES FOR THE CONTROLLERS

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "home#index"
+  resources :doctors
+   resources :users do
+    resources :appointments, only: %i[index create new destroy]
+   end
 end
