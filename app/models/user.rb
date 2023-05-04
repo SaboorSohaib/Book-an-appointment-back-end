d4084ef09443494462f9dd77405e3bf5c192b39e
MudasirSherwani <mudasir9757@gmail.com>
2023-05-04 18:23:12 +0500

    Setup Devise in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
 has_many :appointments, class_name: 'Appointment', foreign_key: 'user_id'
 validates :name, :email, :password, presence: true, length: { minimum: 3, maximum: 50 }
 end
