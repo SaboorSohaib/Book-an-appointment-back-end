035e80dbdb54823b650eb41ca662e3ec6fc77b0a
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-04 18:39:15 +0430

    ADD USER, DOCTOR AND APPOINTMNETS CONTROLLERS

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,32 @@
+class AppointmentsController < ApplicationController
+  def index
+    @user = User.find_by_id(params[:user_id])
+    @appointments = @user.appointments.all
+  end
+
+  def create
+    @user = User.find_by_id(params[:user_id])
+    @appointment = @user.appointments.new(appointment_params)
+    if @appointment.save
+      render :json => @appointment
+    else
+      render :new
+    end
+  end
+
+  def new
+    @appointment = Appointment.new
+  end
+
+  def destroy
+    @user = User.find_by_id(params[:user_id])
+    @appointment = @user.appointments.find_by_id(params[:id])
+    @appointment.destroy
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:disease, :city, :appointment_date, :appointment_time, :user_id, :doctor_id)
+  end
+end
---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,33 @@
+class DoctorsController < ApplicationController
+  def index
+    @doctors = Doctor.all
+  end
+
+  def show
+    @doctor = Doctor.find_by_id(params[:id])
+  end
+
+  def create
+    @doctor = Doctor.new(params[:doctor_params])
+    if @doctor.save
+      render :json => @doctor
+    else
+      render :new
+    end
+  end
+
+  def new
+    @doctors = Doctor.new
+  end
+
+  def destroy
+    @doctor = Doctor.find_by_id(params[:id])
+    @doctor.destroy
+  end
+
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :specialty, :description, :city, :image)
+  end
+end
