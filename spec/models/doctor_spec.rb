c6848b1f73dbc34f1a77873dd34494765314b5a3
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-14 22:03:24 +0430

    ADD TEST FOR DOCTOR MODLE ATTRIBUTS

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Doctor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Doctor.create!(name: 'abid', city: 'kabul', description: 'good doctor', image: 'image', speciality: 'dentist') }
+  before { subject.save }
+
+  it 'should have a name' do
+    expect(subject.name).to eq('abid')
+  end
+
+  it 'should have a city' do
+    expect(subject.city).to eq('kabul')
+  end
+
+  it 'should have a description' do
+    expect(subject.description).to eq('good doctor')
+  end
+
+  it 'should have an image' do
+    expect(subject.image).to eq('image')
+  end
+
+  it 'should have a speciality' do
+    expect(subject.speciality).to eq('dentist')
+  end
+
 end
