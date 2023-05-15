b9c440b831f126685b1ff6580116dedbefa31f1f
Zainab-Alzubair <zainab.a.elzubaie@gmail.com>
2023-05-15 15:31:12 +0300

    Add show reservations integration test

--- spec/integration/appointments_spec.rb
+++ spec/integration/appointments_spec.rb
@@ -17,4 +17,24 @@ RSpec.describe 'appointments', type: :request do
     end
   end
 
+  path '{user_id}/appointments/' do
+    parameter name: 'user_id', in: :path, type: :string, description: 'user_id'
+
+    get('list reservations') do
+      tags 'api/appointments'
+      response(200, 'successful') do
+        let(:user_id) { '1' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
 end
