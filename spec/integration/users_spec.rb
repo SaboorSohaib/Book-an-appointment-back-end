1a4e90316eadaec5325c452b711cc1f0cc3a8fdc
Zainab-Alzubair <zainab.a.elzubaie@gmail.com>
2023-05-15 15:48:33 +0300

    Add List all users in users integration test

--- spec/integration/users_spec.rb
+++ spec/integration/users_spec.rb
@@ -17,4 +17,19 @@ RSpec.describe 'users', type: :request do
     end
   end
 
+  path '/users' do
+    get('list users') do
+      tags 'api/users'
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
 end
