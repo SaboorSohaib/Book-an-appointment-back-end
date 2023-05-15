c8fdf662416cdf85eb0dbf7b87a8e26355c1796c
Zainab-Alzubair <zainab.a.elzubaie@gmail.com>
2023-05-15 17:27:06 +0300

    Remove the update in appointments tes

--- spec/integration/appointments_spec.rb
+++ spec/integration/appointments_spec.rb
@@ -58,40 +58,6 @@ RSpec.describe 'appointments', type: :request do
       end
     end
 
-    patch('update reservation') do
-      tags 'api/appointments'
-      response(200, 'successful') do
-        let(:user_id) { '1' }
-        let(:id) { '1' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
-    put('update reservation') do
-      tags 'api/appointments'
-      response(200, 'successful') do
-        let(:user_id) { '1' }
-        let(:id) { '1' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
     delete('delete reservation') do
       tags 'api/appointments'
       response(200, 'successful') do
